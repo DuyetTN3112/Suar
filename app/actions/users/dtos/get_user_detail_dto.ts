171b1125d6a0ffafce7dca2cc583d1116df29c53
DuyetTN3112 <tranngocduyet31@gmail.com>
2025-06-10 09:39:33 +0700

    feat(ui): consolidate task-application-dtos and update-task-dto (setup-final 44)

--- app/actions/users/dtos/get_user_detail_dto.ts
+++
@@ -1,9 +0,0 @@
-import { IdDTO, type Query } from '../../shared/index.js'
-
-/**
- * GetUserDetailDTO
- *
- * Data Transfer Object for getting user detail by ID.
- * Used by GetUserDetailQuery.
- */
-export class GetUserDetailDTO extends IdDTO implements Query {}
