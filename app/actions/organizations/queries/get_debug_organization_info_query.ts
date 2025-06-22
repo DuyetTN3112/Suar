1ccc93458fa70edca5be2cf018bd6d88e9da5a54
DuyetTN3112 <tranngocduyet31@gmail.com>
2025-06-22 15:41:05 +0700

    chore(repo): consolidate get-all-organizations-query and get-debug-organization-info-query (setup-final 75)

---
+++ app/actions/organizations/queries/get_debug_organization_info_query.ts
@@ -0,0 +1,38 @@
+import UserRepository from '#infra/users/repositories/user_repository'
+import type { DatabaseId } from '#types/database'
+
+interface DebugOrgInfo {
+  user_id: DatabaseId
+  username: string | null
+  user_current_organization_id: DatabaseId | null
+  session_organization_id: string | undefined
+  organizations: Record<string, unknown>[]
+}
+
+/**
+ * Query: Get Debug Organization Info
+ *
+ * Loads user's organizations for debugging purposes.
+ */
+export default class GetDebugOrganizationInfoQuery {
+  private readonly __instanceMarker = true
+
+  static {
+    void new GetDebugOrganizationInfoQuery().__instanceMarker
+  }
+
+  static async execute(
+    userId: DatabaseId,
+    sessionOrgId: string | undefined
+  ): Promise<DebugOrgInfo> {
+    const user = await UserRepository.findWithOrganizations(userId)
+
+    return {
+      user_id: user.id,
+      username: user.username,
+      user_current_organization_id: user.current_organization_id,
+      session_organization_id: sessionOrgId,
+      organizations: user.organizations.map((org) => org.serialize()),
+    }
+  }
+}
---
+++ app/actions/organizations/queries/get_organization_members_page_query.ts
@@ -0,0 +1,55 @@
+import type { ExecutionContext } from '#types/execution_context'
+import type { DatabaseId } from '#types/database'
+import UnauthorizedException from '#exceptions/unauthorized_exception'
+import GetOrganizationMembersQuery from './get_organization_members_query.js'
+import GetPendingRequestsQuery from './get_pending_requests_query.js'
+import GetOrganizationMetadataQuery from './get_organization_metadata_query.js'
+import GetOrganizationBasicInfoQuery from './get_organization_basic_info_query.js'
+import GetOrganizationShowDataQuery from './get_organization_show_data_query.js'
+import { GetOrganizationMembersDTO } from '../dtos/request/get_organization_members_dto.js'
+
+export interface OrganizationMembersPageResult {
+  organization: { id: DatabaseId; name: string } | null
+  members: unknown[]
+  roles: unknown[]
+  userRole: string
+  pendingRequests: unknown[]
+}
+
+/**
+ * Query: Get Organization Members Page Data
+ *
+ * Composite query that aggregates all data needed to render
+ * the organization members management page.
+ */
+export default class GetOrganizationMembersPageQuery {
+  constructor(protected execCtx: ExecutionContext) {}
+
+  async execute(
+    organizationId: DatabaseId,
+    userId: DatabaseId
+  ): Promise<OrganizationMembersPageResult> {
+    const currentUserId = userId
+    if (!currentUserId) {
+      throw new UnauthorizedException()
+    }
+
+    const membersDTO = new GetOrganizationMembersDTO(organizationId, 1, 100, undefined, undefined)
+
+    const [membersResult, pendingRequests, metadata, organization, showData] = await Promise.all([
+      new GetOrganizationMembersQuery(this.execCtx).execute(membersDTO),
+      new GetPendingRequestsQuery(this.execCtx).execute(organizationId),
+      new GetOrganizationMetadataQuery().execute(),
+      GetOrganizationBasicInfoQuery.execute(organizationId),
+      new GetOrganizationShowDataQuery().execute(organizationId, userId),
+    ])
+
+    return {
+      organization,
+      members: membersResult.data,
+      roles: metadata.roles,
+      userRole: showData.userRole,
+      pendingRequests,
+    }
+  }
+}
