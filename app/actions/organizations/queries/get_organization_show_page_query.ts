870bd093ea02ba4a5310f05fdc1c46699e20db24
DuyetTN3112 <tranngocduyet31@gmail.com>
2025-07-16 08:39:36 +0700

    refactor(organizations): add get all organizations query and get...
    Details: Add get all organizations query, get debug organization info query, and get organization basic info query.
    Scope: organizations; 13 files, +878/-0 lines.

---
+++ app/actions/organizations/queries/get_organization_show_page_query.ts
@@ -0,0 +1,42 @@
+import type { ExecutionContext } from '#types/execution_context'
+import type { DatabaseId } from '#types/database'
+import GetOrganizationDetailQuery from './get_organization_detail_query.js'
+import GetOrganizationShowDataQuery from './get_organization_show_data_query.js'
+import { GetOrganizationDetailDTO } from '../dtos/request/get_organization_detail_dto.js'
+
+export interface OrganizationShowPageResult {
+  organization: Awaited<ReturnType<GetOrganizationDetailQuery['execute']>>
+  members: Array<{
+    id: DatabaseId
+    username: string
+    email: string
+    org_role: string
+    role_name: string
+  }>
+  userRole: string
+}
+
+/**
+ * Composite Query: Get Organization Show Page Data
+ *
+ * Aggregates all data needed to render the organization show page
+ * by running GetOrganizationDetailQuery and GetOrganizationShowDataQuery in parallel.
+ */
+export default class GetOrganizationShowPageQuery {
+  constructor(protected execCtx: ExecutionContext) {}
+
+  async execute(organizationId: string, userId: DatabaseId): Promise<OrganizationShowPageResult> {
+    const dto = new GetOrganizationDetailDTO(organizationId, true, false, false)
+
+    const [organization, showData] = await Promise.all([
+      new GetOrganizationDetailQuery(this.execCtx).execute(dto),
+      new GetOrganizationShowDataQuery().execute(organizationId, userId),
+    ])
+
+    return {
+      organization,
+      members: showData.members,
+      userRole: showData.userRole,
+    }
+  }
+}
