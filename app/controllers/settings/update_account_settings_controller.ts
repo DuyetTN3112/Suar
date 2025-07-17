bef538fb09a35d37c4f95c1e0567f1604d27bb4c
DuyetTN3112 <tranngocduyet31@gmail.com>
2025-07-17 10:47:16 +0700

    refactor(settings): extract settings commands

--- app/controllers/settings/update_account_settings_controller.ts
+++ app/controllers/settings/update_account_settings_controller.ts
@@ -3,7 +3,7 @@ import type { HttpContext } from '@adonisjs/core/http'
 import { buildUpdateAccountSettingsDTO } from './mappers/request/settings_request_mapper.js'
 import { getAccountSettingsUpdatedMessage } from './mappers/response/settings_response_mapper.js'
 
-import UpdateUserProfileCommand from '#actions/users/commands/update_user_profile_command'
+import UpdateAccountSettingsCommand from '#actions/settings/commands/update_account_settings_command'
 import UnauthorizedException from '#exceptions/unauthorized_exception'
 import { ExecutionContext } from '#types/execution_context'
 
@@ -19,7 +19,7 @@ export default class UpdateAccountSettingsController {
       throw new UnauthorizedException()
     }
     const dto = buildUpdateAccountSettingsDTO(request, user.id, user.email)
-    const command = new UpdateUserProfileCommand(ExecutionContext.fromHttp(ctx))
+    const command = new UpdateAccountSettingsCommand(ExecutionContext.fromHttp(ctx))
     await command.handle(dto)
 
     session.flash('success', getAccountSettingsUpdatedMessage())
