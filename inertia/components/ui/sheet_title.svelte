3d927863b0523229e29c18830e065891b9ac0f27
DuyetTN3112 <tranngocduyet31@gmail.com>
2025-07-31 14:32:01 +0700

    refactor(ui): add sheet and sheet close
    Details: Add sheet, sheet close, and sheet content.
    Scope: ui; 27 files, +900/-0 lines.

---
+++ inertia/components/ui/sheet_title.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { cn } from '$lib/utils-svelte'
+  import { Dialog as SheetPrimitive, type DialogTitleProps } from 'bits-ui'
+
+  type Props = DialogTitleProps
+
+  const props: Props = $props()
+  const className = $derived(props.class)
+  const children = $derived(props.children)
+  const restProps = $derived.by(() => {
+    const { class: _className, children: _children, ...rest } = props
+    return rest
+  })
+</script>
+
+<SheetPrimitive.Title
+  class={cn('text-foreground font-semibold', className)}
+  {...restProps}
+>
+  {@render children?.()}
+</SheetPrimitive.Title>
---
+++ inertia/components/ui/sheet_trigger.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { Dialog as SheetPrimitive, type DialogTriggerProps } from 'bits-ui'
+
+  type Props = DialogTriggerProps
+
+  const props: Props = $props()
+  const children = $derived(props.children)
+  const restProps = $derived.by(() => {
+    const { children: _children, ...rest } = props
+    return rest
+  })
+</script>
+
+<SheetPrimitive.Trigger {...restProps}>
+  {@render children?.()}
+</SheetPrimitive.Trigger>
