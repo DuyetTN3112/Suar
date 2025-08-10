0d0b7f3b4b8d1cf3400c49d0238ad92f9e66ca00
DuyetTN3112 <tranngocduyet31@gmail.com>
2025-08-10 17:32:17 +0700

    actions: get_debug_organization_info_query

--- app/actions/http/queries/get_debug_organization_info_query.ts
+++
@@ -1,8 +0,0 @@
-import GetDebugOrganizationInfoQuery from '#actions/organizations/queries/get_debug_organization_info_query'
-import type { DatabaseId } from '#types/database'
-
-export default class GetHttpDebugOrganizationInfoQuery {
-  async execute(userId: DatabaseId, sessionOrgId: string | undefined) {
-    return GetDebugOrganizationInfoQuery.execute(userId, sessionOrgId)
-  }
-}
