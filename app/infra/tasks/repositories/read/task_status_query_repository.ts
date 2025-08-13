fd945f9bbc20d1ab6b90b3685ef462c0570c2622
DuyetTN3112 <tranngocduyet31@gmail.com>
2025-08-13 20:41:52 +0700

    chore(repo): consolidate skill-queries and user-skill-queries (setup-final 192)

--- app/infra/tasks/repositories/read/task_status_query_repository.ts
+++
@@ -1,9 +0,0 @@
-import type { TaskStatusQueryRepositoryPort } from '#actions/tasks/ports/task_status_query_repository_port'
-import TaskStatusRepository from '#infra/tasks/repositories/task_status_repository'
-
-export const taskStatusQueryRepository: TaskStatusQueryRepositoryPort = {
-  async findByIdAndOrgActive(statusId, organizationId, trx) {
-    return TaskStatusRepository.findByIdAndOrgActive(statusId, organizationId, trx)
-  },
-}
-
--- app/infra/tasks/repositories/write/task_assignment_command_repository.ts
+++
@@ -1,9 +0,0 @@
-import { completeActiveAssignmentsForCompletedTask } from './task_assignment_mutations.js'
-
-import type { TaskAssignmentCommandRepositoryPort } from '#actions/tasks/ports/task_assignment_command_repository_port'
-
-
-export const taskAssignmentCommandRepository: TaskAssignmentCommandRepositoryPort = {
-  completeActiveAssignmentsForCompletedTask,
-}
-
