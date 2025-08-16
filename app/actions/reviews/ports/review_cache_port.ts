91b2d718e4c14fbfbdc8d7e9f6f8b86ebfa804eb
DuyetTN3112 <tranngocduyet31@gmail.com>
2025-08-16 18:39:24 +0700

    chore(repo): consolidate assignment-completion-listener and review-application-mapper (setup-final 199)

--- app/actions/reviews/ports/review_cache_port.ts
+++
@@ -1,7 +0,0 @@
-import type { DatabaseId } from '#types/database'
-
-export interface ReviewCachePort {
-  invalidateReview(reviewId: DatabaseId): Promise<void>
-  invalidateUserReviewData(userId: DatabaseId): Promise<void>
-  invalidateUserProfileReviewData(userId: DatabaseId): Promise<void>
-}
