95389ee30a4d2ee7e665989b2c36d6323e8f1075
DuyetTN3112 <tranngocduyet31@gmail.com>
2025-08-19 16:21:19 +0700

    actions: create_project_command

--- app/actions/organizations/current/projects/commands/create_project_command.ts
+++ app/actions/organizations/current/projects/commands/create_project_command.ts
@@ -1,11 +1,10 @@
-import type { CreateProjectDTO } from '#actions/projects/dtos/request/create_project_dto'
-import CreateProjectCommand from '#actions/projects/commands/create_project_command'
+import { projectPublicApi, type CreateProjectDTO } from '#actions/projects/public_api'
 import type { ExecutionContext } from '#types/execution_context'
 
 export default class CreateCurrentOrganizationProjectCommand {
   constructor(protected execCtx: ExecutionContext) {}
 
   async handle(dto: CreateProjectDTO) {
-    return new CreateProjectCommand(this.execCtx).handle(dto)
+    return projectPublicApi.createProject(dto, this.execCtx)
   }
 }
