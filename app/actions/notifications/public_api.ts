cce1fd2fdeabeeae928e354037fe9b1b1939f33f
DuyetTN3112 <tranngocduyet31@gmail.com>
2025-08-21 20:15:12 +0700

    chore(repo): consolidate base-command and base-query (setup-final 210)

--- app/actions/notifications/public_api.ts
+++
@@ -1,2 +0,0 @@
-export { NotificationPublicApi, notificationPublicApi } from './services/notification_public_api.js'
-export type { NotificationCreator, NotificationData } from './create_notification.js'
--- app/actions/projects/public_api.ts
+++
@@ -1,2 +0,0 @@
-export { ProjectPublicApi, projectPublicApi } from './services/project_public_api.js'
-export type { CreateProjectDTO } from './dtos/request/create_project_dto.js'
