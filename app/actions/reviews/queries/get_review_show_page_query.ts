14df5093bb0cac38114a6390110828c4fb23c542
DuyetTN3112 <tranngocduyet31@gmail.com>
2025-08-29 08:59:53 +0700

    refactor(projects): add project external dependencies impl and get...
    Details: Add project external dependencies impl, get project create page query, and review external dependencies.
    Scope: projects; 10 files, +696/-0 lines.

---
+++ app/actions/reviews/queries/get_review_show_page_query.ts
@@ -0,0 +1,30 @@
+import GetReviewSessionQuery from './get_review_session_query.js'
+
+import { GetReviewSessionDTO } from '#actions/reviews/dtos/request/review_dtos'
+import GetActiveSkillsQuery from '#actions/shared/queries/get_active_skills_query'
+import { proficiencyLevelOptions } from '#constants/user_constants'
+import type { DatabaseId } from '#types/database'
+import type { ExecutionContext } from '#types/execution_context'
+
+export interface GetReviewShowPageResult {
+  session: Awaited<ReturnType<GetReviewSessionQuery['handle']>>
+  skills: Awaited<ReturnType<typeof GetActiveSkillsQuery.execute>>
+  proficiencyLevels: typeof proficiencyLevelOptions
+}
+
+export default class GetReviewShowPageQuery {
+  constructor(protected execCtx: ExecutionContext) {}
+
+  async execute(reviewSessionId: DatabaseId): Promise<GetReviewShowPageResult> {
+    const [session, skills] = await Promise.all([
+      new GetReviewSessionQuery(this.execCtx).handle(new GetReviewSessionDTO(reviewSessionId)),
+      GetActiveSkillsQuery.execute(),
+    ])
+
+    return {
+      session,
+      skills,
+      proficiencyLevels: proficiencyLevelOptions,
+    }
+  }
+}
