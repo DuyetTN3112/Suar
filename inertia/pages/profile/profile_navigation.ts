0a88637b3b22eb2c06626a4a1afb3967cb9275ff
DuyetTN3112 <tranngocduyet31@gmail.com>
2025-10-08 12:50:19 +0700

    feat(ui): consolidate index and organization-debug (setup-final 325)

--- inertia/pages/profile/profile_navigation.ts
+++
@@ -1,7 +0,0 @@
-export function navigateToProfileEdit(): void {
-  window.location.assign('/profile/edit')
-}
-
-export function navigateToUserReviews(userId: string): void {
-  window.location.assign(`/users/${userId}/reviews`)
-}
