90b3a0f899c2fd7f34722c355a141fda26a5ebca
DuyetTN3112 <tranngocduyet31@gmail.com>
2025-10-10 13:28:10 +0700

    feat(ui): consolidate status-board and status-board-policy (setup-final 331)

--- inertia/pages/tasks/status_board_types.ts
+++
@@ -1,7 +0,0 @@
-import type {
-  SliceItem as GeneratedSliceItem,
-  SliceMetadata as GeneratedSliceMetadata,
-} from '~/generated/data'
-
-export type SliceItem = GeneratedSliceItem
-export type SliceMetadata = GeneratedSliceMetadata
