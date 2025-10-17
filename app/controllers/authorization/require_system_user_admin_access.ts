f851fd83372b5ddc8b8692c268284e62f439e7b7
DuyetTN3112 <tranngocduyet31@gmail.com>
2025-10-17 15:30:53 +0700

    controllers: require_system_user_admin_access

--- app/controllers/authorization/require_system_user_admin_access.ts
+++ app/controllers/authorization/require_system_user_admin_access.ts
@@ -1,6 +1,6 @@
 import type { HttpContext } from '@adonisjs/core/http'
 
-import AuthorizeSystemUserAdminAccessQuery from '#actions/authorization/queries/authorize_system_user_admin_access_query'
+import { AuthorizeSystemUserAdminAccessQuery } from '#actions/authorization/public_api'
 import { ErrorMessages } from '#constants/error_constants'
 import BusinessLogicException from '#exceptions/business_logic_exception'
 import UnauthorizedException from '#exceptions/unauthorized_exception'
