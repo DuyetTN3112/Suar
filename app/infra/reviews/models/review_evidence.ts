088be4b536d9a823ecda367decd95b95b2397c25
DuyetTN3112 <tranngocduyet31@gmail.com>
2025-11-16 14:53:22 +0700

    infra: review_evidence

---
+++ app/infra/reviews/models/review_evidence.ts
@@ -0,0 +1,44 @@
+import { BaseModel, column, belongsTo } from '@adonisjs/lucid/orm'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+import { DateTime } from 'luxon'
+
+import ReviewSession from './review_session.js'
+
+import User from '#infra/users/models/user'
+
+export default class ReviewEvidence extends BaseModel {
+  static override table = 'review_evidences'
+
+  @column({ isPrimary: true })
+  declare id: string
+
+  @column()
+  declare review_session_id: string
+
+  @column()
+  declare evidence_type: string
+
+  @column()
+  declare url: string | null
+
+  @column()
+  declare title: string | null
+
+  @column()
+  declare description: string | null
+
+  @column()
+  declare uploaded_by: string | null
+
+  @column.dateTime({ autoCreate: true })
+  declare created_at: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updated_at: DateTime
+
+  @belongsTo(() => ReviewSession, { foreignKey: 'review_session_id' })
+  declare reviewSession: BelongsTo<typeof ReviewSession>
+
+  @belongsTo(() => User, { foreignKey: 'uploaded_by' })
+  declare uploader: BelongsTo<typeof User>
+}
