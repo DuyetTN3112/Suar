163e8dabbfa98aa3246b2e85d14222f8e15539c0
DuyetTN3112 <tranngocduyet31@gmail.com>
2026-03-18 10:37:01 +0700

    refactor(users): normalize controller adapters

--- app/modules/users/controllers/show_user_controller.ts
+++ app/modules/users/controllers/show_user_controller.ts
@@ -5,6 +5,7 @@ import { buildGetUserDetailDTO } from './mappers/request/user_request_mapper.js'
 import { mapShowUserPageProps } from './mappers/response/user_response_mapper.js'
 
 import { actionContextFromHttp } from '#modules/http/adapters/http_execution_context_adapter'
+import ForbiddenException from '#modules/http/exceptions/forbidden_exception'
 import GetUserDetailQuery from '#modules/users/actions/queries/get_user_detail_query'
 
 /**
@@ -13,10 +14,17 @@ import GetUserDetailQuery from '#modules/users/actions/queries/get_user_detail_q
 export default class ShowUserController {
   async handle(ctx: HttpContext) {
     const getUserDetailQuery = new GetUserDetailQuery(actionContextFromHttp(ctx))
-    const { params, inertia } = ctx
+    const { params, inertia, auth } = ctx
 
     const dto = buildGetUserDetailDTO(String(params.id))
     const user = await getUserDetailQuery.handle(dto)
+
+    const requestingUser = auth.getUserOrFail()
+    if (requestingUser.id !== user.id && requestingUser.system_role !== 'superadmin' && requestingUser.system_role !== 'system_admin') {
+      if (requestingUser.current_organization_id !== user.current_organization_id) {
+        throw new ForbiddenException('Bạn không có quyền xem thông tin người dùng này')
+      }
+    }
 
     return inertia.render('users/show', mapShowUserPageProps(user))
   }
