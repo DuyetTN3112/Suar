f630d4b4e7d285bc2faa29e89973e9d9e61876d7
DuyetTN3112 <tranngocduyet31@gmail.com>
2026-03-27 21:34:44 +0700

    refactor(reviews): normalize review controller mappers

--- app/modules/reviews/controllers/get_task_self_assessment_controller.ts
+++ app/modules/reviews/controllers/get_task_self_assessment_controller.ts
@@ -2,6 +2,7 @@ import type { HttpContext } from '@adonisjs/core/http'
 
 import { mapTaskSelfAssessmentApiBody } from './mappers/response/review_response_mapper.js'
 
+import { actionContextFromHttp } from '#modules/http/adapters/http_execution_context_adapter'
 import GetTaskSelfAssessmentQuery from '#modules/reviews/actions/queries/get_task_self_assessment_query'
 
 /**
@@ -10,7 +11,7 @@ import GetTaskSelfAssessmentQuery from '#modules/reviews/actions/queries/get_tas
 export default class GetTaskSelfAssessmentController {
   async handle(ctx: HttpContext) {
     const { response, params } = ctx
-    const query = new GetTaskSelfAssessmentQuery()
+    const query = new GetTaskSelfAssessmentQuery(actionContextFromHttp(ctx))
     const data = await query.execute(params.id as string)
 
     response.status(200).json(mapTaskSelfAssessmentApiBody(data))
