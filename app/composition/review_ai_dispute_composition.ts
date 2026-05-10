d2ab492b30fda34ee0ed48946d849fe04a91f67b
DuyetTN3112 <tranngocduyet31@gmail.com>
2026-05-10 22:29:29 +0700

    refactor(composition): consolidate projects-search-composition and review-action-factory (setup-final 824)

--- app/composition/review_ai_dispute_composition.ts
+++
@@ -1,8 +0,0 @@
-import ReconcileAiDisputeEvaluationsCommand from '#modules/reviews/actions/commands/reconcile_ai_dispute_evaluations_command'
-import { LucidAiDisputeEvaluationGateway } from '#modules/reviews/infra/adapters/lucid_ai_dispute_evaluation_gateway'
-
-export const aiDisputeEvaluationGateway = LucidAiDisputeEvaluationGateway.fromEnvironment()
-
-export const reconcileAiDisputeEvaluationsCommand = new ReconcileAiDisputeEvaluationsCommand(
-  aiDisputeEvaluationGateway
-)
