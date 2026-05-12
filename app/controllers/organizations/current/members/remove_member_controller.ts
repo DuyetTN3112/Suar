3bf3c6f76e3ac1bf8656d968e4150f33682caf66
NgocDuyet <td6622i@gre.ac.uk>
2026-05-12 23:06:01 +0700

    controllers: remove_member_controller

--- app/controllers/organizations/current/members/remove_member_controller.ts
+++ app/controllers/organizations/current/members/remove_member_controller.ts
@@ -1,6 +1,6 @@
 import type { HttpContext } from '@adonisjs/core/http'
 
-import CreateNotification from '#actions/common/create_notification'
+import { notificationPublicApi } from '#actions/notifications/public_api'
 import RemoveMemberCommand from '#actions/organizations/commands/remove_member_command'
 import { ErrorMessages } from '#constants/error_constants'
 import { buildCurrentOrganizationRemoveMemberDTO } from '#controllers/organizations/current/mappers/request/current_organization_mutation_request_mapper'
@@ -31,7 +31,7 @@ export default class RemoveMemberController {
       params.id as string
     )
 
-    await new RemoveMemberCommand(execCtx, new CreateNotification()).execute(dto)
+    await new RemoveMemberCommand(execCtx, notificationPublicApi).execute(dto)
 
     if (request.accepts(['html', 'json']) === 'json') {
       response.json(mapCurrentOrganizationSuccessApiBody('Xóa thành viên thành công'))
