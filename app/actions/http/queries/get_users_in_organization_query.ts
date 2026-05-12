970f349aefea7b064f798f5150ef3363948fa707
NgocDuyet <td6622i@gre.ac.uk>
2026-05-12 23:05:57 +0700

    actions: get_users_in_organization_query

--- app/actions/http/queries/get_users_in_organization_query.ts
+++
@@ -1,8 +0,0 @@
-import GetUsersInOrganizationQuery from '#actions/organizations/queries/get_users_in_organization_query'
-import type { DatabaseId } from '#types/database'
-
-export default class GetHttpUsersInOrganizationQuery {
-  async execute(organizationId: DatabaseId, excludeUserId: DatabaseId) {
-    return new GetUsersInOrganizationQuery().execute(organizationId, excludeUserId)
-  }
-}
