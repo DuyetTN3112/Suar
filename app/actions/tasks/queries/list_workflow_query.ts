6084ba6c2851ee8c2f62d6cf479482b420dbfea1
NgocDuyet <td6622i@gre.ac.uk>
2026-05-12 23:06:00 +0700

    actions: list_workflow_query

--- app/actions/tasks/queries/list_workflow_query.ts
+++ app/actions/tasks/queries/list_workflow_query.ts
@@ -1,13 +1,13 @@
 import TaskWorkflowTransitionRepository from '#infra/tasks/repositories/task_workflow_transition_repository'
-import type TaskWorkflowTransition from '#models/task_workflow_transition'
 import type { DatabaseId } from '#types/database'
+import type { TaskWorkflowTransitionRecord } from '#types/task_records'
 
 /**
  * Query: List all workflow transitions for an organization.
  * Returns transitions with preloaded from/to status names.
  */
 export default class ListWorkflowQuery {
-  async execute(organizationId: DatabaseId): Promise<TaskWorkflowTransition[]> {
+  async execute(organizationId: DatabaseId): Promise<TaskWorkflowTransitionRecord[]> {
     return TaskWorkflowTransitionRepository.findByOrganization(organizationId)
   }
 }
