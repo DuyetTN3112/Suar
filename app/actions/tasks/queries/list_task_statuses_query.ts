3bfd9e5e16fe3f0b618ccdbc5b3c12e694163c71
NgocDuyet <td6622i@gre.ac.uk>
2026-05-12 23:06:00 +0700

    actions: list_task_statuses_query

--- app/actions/tasks/queries/list_task_statuses_query.ts
+++ app/actions/tasks/queries/list_task_statuses_query.ts
@@ -1,12 +1,12 @@
 import TaskStatusRepository from '#infra/tasks/repositories/task_status_repository'
-import type TaskStatus from '#models/task_status'
 import type { DatabaseId } from '#types/database'
+import type { TaskStatusRecord } from '#types/task_records'
 
 /**
  * Query: List all active task statuses for an organization.
  */
 export default class ListTaskStatusesQuery {
-  async execute(organizationId: DatabaseId): Promise<TaskStatus[]> {
+  async execute(organizationId: DatabaseId): Promise<TaskStatusRecord[]> {
     return TaskStatusRepository.findByOrganization(organizationId)
   }
 }
