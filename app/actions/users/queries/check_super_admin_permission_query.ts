acdc6baa9c699e937b4e2ca6ff27a4ce408c65c1
NgocDuyet <td6622i@gre.ac.uk>
2026-05-12 23:06:01 +0700

    actions: check_super_admin_permission_query

--- app/actions/users/queries/check_super_admin_permission_query.ts
+++ app/actions/users/queries/check_super_admin_permission_query.ts
@@ -1,2 +1,2 @@
 // Backward-compatibility alias: kept to avoid breaking older imports.
-export { default } from '#actions/authorization/queries/authorize_system_user_admin_access_query'
+export { AuthorizeSystemUserAdminAccessQuery as default } from '#actions/authorization/public_api'
