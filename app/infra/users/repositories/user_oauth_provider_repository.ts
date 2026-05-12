a463466d85ceb9b83e7b21482523d0a2c0699b1a
NgocDuyet <td6622i@gre.ac.uk>
2026-05-12 23:06:05 +0700

    infra: user_oauth_provider_repository

--- app/infra/users/repositories/user_oauth_provider_repository.ts
+++ app/infra/users/repositories/user_oauth_provider_repository.ts
@@ -1,6 +1,6 @@
 import type { TransactionClientContract } from '@adonisjs/lucid/types/database'
 
-import UserOAuthProvider from '#models/user_oauth_provider'
+import UserOAuthProvider from '#infra/users/models/user_oauth_provider'
 
 export default class UserOAuthProviderRepository {
   private readonly __instanceMarker = true
