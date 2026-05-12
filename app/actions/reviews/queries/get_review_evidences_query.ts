ac59e1bea90e90aec88baa10068a241a537c7a26
NgocDuyet <td6622i@gre.ac.uk>
2026-05-12 23:05:58 +0700

    actions: get_review_evidences_query

--- app/actions/reviews/queries/get_review_evidences_query.ts
+++ app/actions/reviews/queries/get_review_evidences_query.ts
@@ -1,11 +1,12 @@
 import ReviewEvidenceRepository from '#infra/reviews/repositories/review_evidence_repository'
 import type { DatabaseId } from '#types/database'
+import type { ReviewEvidenceRecord } from '#types/review_records'
 
 /**
  * Query: list evidences for a review session.
  */
 export default class GetReviewEvidencesQuery {
-  async execute(reviewSessionId: DatabaseId): Promise<import('#models/review_evidence').default[]> {
+  async execute(reviewSessionId: DatabaseId): Promise<ReviewEvidenceRecord[]> {
     return ReviewEvidenceRepository.listBySession(reviewSessionId)
   }
 }
