f6fedf3805ae49e21ab8c5b0bf6ad01ef4c578e6
NgocDuyet <td6622i@gre.ac.uk>
2026-05-12 23:05:57 +0700

    actions: clear_cache_key_command

---
+++ app/actions/http/cache/commands/clear_cache_key_command.ts
@@ -0,0 +1,18 @@
+import { ErrorMessages } from '#constants/error_constants'
+import BusinessLogicException from '#exceptions/business_logic_exception'
+import CacheService from '#infra/cache/cache_service'
+import type { ExecutionContext } from '#types/execution_context'
+
+export default class ClearCacheKeyCommand {
+  constructor(protected execCtx: ExecutionContext) {}
+
+  async execute(key: string): Promise<void> {
+    void this.execCtx
+
+    if (!key) {
+      throw new BusinessLogicException(ErrorMessages.INVALID_INPUT)
+    }
+
+    await CacheService.delete(key)
+  }
+}
