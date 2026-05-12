a81d0da2d5f087c171e1f3a9a08f44a0d66bc036
NgocDuyet <td6622i@gre.ac.uk>
2026-05-12 23:06:05 +0700

    infra: user_work_history_repository

--- app/infra/users/repositories/user_work_history_repository.ts
+++ app/infra/users/repositories/user_work_history_repository.ts
@@ -1,6 +1,6 @@
 import type { TransactionClientContract } from '@adonisjs/lucid/types/database'
 
-import UserWorkHistory from '#models/user_work_history'
+import UserWorkHistory from '#infra/users/models/user_work_history'
 import type { DatabaseId } from '#types/database'
 
 export default class UserWorkHistoryRepository {
