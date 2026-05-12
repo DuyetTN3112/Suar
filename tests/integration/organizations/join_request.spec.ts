04dd27ad59cfed844750cfaec263ab31f16ee54e
NgocDuyet <td6622i@gre.ac.uk>
2026-05-12 23:06:08 +0700

    test: join_request.spec

--- tests/integration/organizations/join_request.spec.ts
+++ tests/integration/organizations/join_request.spec.ts
@@ -1,8 +1,8 @@
 import { test } from '@japa/runner'
 
 import { OrganizationRole, OrganizationUserStatus } from '#constants/organization_constants'
+import OrganizationUser from '#infra/organizations/models/organization_user'
 import OrganizationUserRepository from '#infra/organizations/repositories/organization_user_repository'
-import OrganizationUser from '#models/organization_user'
 import { setupApp, teardownApp } from '#tests/helpers/bootstrap'
 import { UserFactory, OrganizationFactory, cleanupTestData } from '#tests/helpers/factories'
 
