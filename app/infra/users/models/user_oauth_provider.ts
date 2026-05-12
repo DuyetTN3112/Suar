349e98605a652961ae653ec63acea067b2473984
NgocDuyet <td6622i@gre.ac.uk>
2026-05-12 23:06:05 +0700

    infra: user_oauth_provider

---
+++ app/infra/users/models/user_oauth_provider.ts
@@ -0,0 +1,41 @@
+import { BaseModel, belongsTo, column } from '@adonisjs/lucid/orm'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+import { DateTime } from 'luxon'
+
+import User from './user.js'
+
+export default class UserOAuthProvider extends BaseModel {
+  static override table = 'user_oauth_providers'
+
+  @column({ isPrimary: true })
+  declare id: string
+
+  @column()
+  declare user_id: string
+
+  @column()
+  declare provider: string
+
+  @column()
+  declare provider_id: string
+
+  @column()
+  declare email: string | null
+
+  @column()
+  declare access_token: string | null
+
+  @column()
+  declare refresh_token: string | null
+
+  @column.dateTime({ autoCreate: true })
+  declare created_at: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updated_at: DateTime
+
+  @belongsTo(() => User, {
+    foreignKey: 'user_id',
+  })
+  declare user: BelongsTo<typeof User>
+}
