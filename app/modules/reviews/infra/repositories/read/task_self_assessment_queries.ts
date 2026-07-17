6e01b5fd8d94e41a95ddf2ae1b63214faf653ff0
DuyetTN3112 <tranngocduyet31@gmail.com>
2026-07-17 15:20:12 +0700

    chore(repo): consolidate task-review-board-queries and task-self-assessment-queries (setup-final 988)

--- app/modules/reviews/infra/repositories/read/task_self_assessment_queries.ts
+++ app/modules/reviews/infra/repositories/read/task_self_assessment_queries.ts
@@ -1,6 +1,6 @@
 import type { TransactionClientContract } from '@adonisjs/lucid/types/database'
 
-import TaskSelfAssessment from '../../models/task_self_assessment.js'
+import TaskSelfAssessment from '../../models/self-assessment/task_self_assessment.js'
 
 const baseQuery = (trx?: TransactionClientContract) => {
   return trx ? TaskSelfAssessment.query({ client: trx }) : TaskSelfAssessment.query()
