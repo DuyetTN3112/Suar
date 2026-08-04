ff7936bb967c13360969307ad1d2af90d410dbee
DuyetTN3112 <tranngocduyet31@gmail.com>
2026-08-04 23:16:32 +0700

    chore(repo): consolidate get-task-detail-query and get-task-edit-page-query
    Scope: app.
    
    Changes:
    - Update app/modules/tasks/actions/queries/get_task_detail_query.ts
    - Update app/modules/tasks/actions/queries/get_task_edit_page_query.ts
    - Update app/modules/tasks/actions/queries/get_task_metadata_query.ts
    - Update app/modules/tasks/actions/queries/get_task_projects_query.ts
    - Update app/modules/tasks/actions/queries/get_task_statistics_query.ts
    - Update app/modules/tasks/actions/queries/get_task_status_query.ts
    - Update app/modules/tasks/actions/queries/get_task_submission_query.ts
    - Update app/modules/tasks/actions/queries/get_tasks_grouped_query.ts
    - Update 12 additional file(s) in the same change set
    
    Files changed: 20
    Line delta: +13 / -1983

--- app/modules/tasks/actions/queries/list_task_statuses_query.ts
+++ app/modules/tasks/actions/queries/list_task_statuses_query.ts
@@ -1,13 +1,8 @@
 import type { TaskStatusQueryRepositoryPort } from '#modules/tasks/actions/ports/outbound/task_status_query_repository_port'
 import type { TaskStatusRecord } from '#modules/tasks/types/task_records'
 
-/**
- * Query: List all active task statuses for an organization.
- */
 export default class ListTaskStatusesQuery {
-  constructor(
-    private readonly repository: Pick<TaskStatusQueryRepositoryPort, 'findByOrganization'>
-  ) {}
+  constructor(private readonly repository: Pick<TaskStatusQueryRepositoryPort, 'findByOrganization'>) {}
 
   async execute(organizationId: string): Promise<TaskStatusRecord[]> {
     return this.repository.findByOrganization(organizationId)
